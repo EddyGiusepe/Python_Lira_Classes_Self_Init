55275006beafac779391700e8d1a86c82eecc728
EddyGiusepe <eddychirinos.unac@gmail.com>
2022-02-21 20:52:58 -0300

    Arquivo_7

--- lembrando_comandos_python/treinando_com_python_7.py
+++ lembrando_comandos_python/treinando_com_python_7.py
@@ -3,9 +3,8 @@ Data Scientist Jr.: Dr.Eddy Giusepe Chirinos Isidro
 
 Link de estudo: https://www.youtube.com/watch?v=E76ErVQ1xNI&list=PLqlQ2-9ypflT5Lx864RXtJx4s7i3sxgs0&index=16
 '''
-
 # Python: Herença
 '''
 A herença é a capacidade que tem uma classe de heredar os ATRIBUTOS e MÉTODOS de outra, isso nos ajuda a reutilizar
-código e fazer que nossos programas sejam muito mais ótimos. 
+código e fazer que nossos programas sejam muito mais ótimos.  
 '''
